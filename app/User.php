66ae6839ab1415224809e0d81d72dd60bb234d22
jakub41 <lemiszewski@gmx.com>
2019-04-11 16:55:09 +0200

    User entity: Added User model with user table migration

--- app/User.php
+++ app/User.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
@@ -10,7 +12,7 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
+    use Authenticatable, Authorizable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +22,11 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
     protected $fillable = [
         'name', 'email',
     ];
+
+    public function teams(): BelongsToMany
+    {
+      return $this->belongsToMany(Team::class, 'users_teams');
+    }
 
     /**
      * The attributes excluded from the model's JSON form.
